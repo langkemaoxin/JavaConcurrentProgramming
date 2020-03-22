7905e8e034483796e7cfc972c29e1ac294fa5bd3
浪客猫心 <2363613998@qq.com>
2020-03-22 23:34:40 +0800

    ExecutorCompletionService阻塞获取Future信息，但是执行时乱序的

--- src/main/java/chapter6/T6_2_ExecutorCompletionService_0/Run.java
+++ src/main/java/chapter6/T6_2_ExecutorCompletionService_0/Run.java
@@ -1,4 +1,4 @@
-package chapter5.T5_10_FureList;
+package chapter6.T6_2_ExecutorCompletionService_0;
 
 import java.util.ArrayList;
 import java.util.concurrent.*;
@@ -7,11 +7,17 @@ import java.util.concurrent.*;
  * @ClassName Run
  * @Description TODO
  * @Author GY.C
- * @Date 2020/3/22 23:02
+ * @Date 2020/3/22 23:18
  * @Version 1.0
  *
- * Future的默认实现类 是 FutureTask
- * 这个类有一个缺点，那就是在进行取值的时候是，阻塞型的
+ * 1. 新建一个线程池
+ * 2. 把线程池丢给ExecutorCompletionService
+ * 3. 把所有的任务用 ExecutorCompletionService提交
+ * 4. 阻塞性的获取返回结果，【返回结果都是乱序的，都是同时进行的】
+ *
+ * 获取一个Future对象
+ * ExecutorCompletionService.take().get()
+ *
  *
  */
 public class Run {
@@ -33,20 +39,28 @@ public class Run {
 
         ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
 
-        for (int i = 0; i < 5; i++) {
-            futures.add(poolExecutor.submit(callables.get(i)));
-        }
-
-        System.out.println("run first time="+System.currentTimeMillis());
+        //把线程池对象丢给
+        //把线程池对象丢给 ExecutorCompletionService
+        ExecutorCompletionService csRef = new ExecutorCompletionService(poolExecutor);
 
         for (int i = 0; i < 5; i++) {
-            System.out.println(futures.get(i).get()+" "+System.currentTimeMillis());
+            //然后把callable任务 提交了
+            csRef.submit(callables.get(i));
         }
+
+        System.out.println("run first time=" + System.currentTimeMillis());
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println("正在打印"+i+"个");
+            System.out.println(csRef.take().get());
+        }
+
+        System.out.println("全部结束了");
 
     }
 }
 
-class MyCallable implements Callable<String>{
+class MyCallable implements Callable<String> {
 
     private String username;
     private long sleepValue;
@@ -60,6 +74,6 @@ class MyCallable implements Callable<String>{
     public String call() throws Exception {
         System.out.println(username);
         Thread.sleep(sleepValue);
-        return "return "+username+System.currentTimeMillis();
+        return "return " + username+System.currentTimeMillis();
     }
 }
