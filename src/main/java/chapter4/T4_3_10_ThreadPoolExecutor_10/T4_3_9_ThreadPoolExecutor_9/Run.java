1766b23dec15241a1905545c273989274ba2e01a
浪客猫心 <2363613998@qq.com>
2020-03-22 11:08:06 +0800

    getCompletedTaskCount 获取已经完成的任务

---
+++ src/main/java/chapter4/T4_3_10_ThreadPoolExecutor_10/T4_3_9_ThreadPoolExecutor_9/Run.java
@@ -0,0 +1,35 @@
+package chapter4.T4_3_10_ThreadPoolExecutor_10.T4_3_9_ThreadPoolExecutor_9;
+
+import Common.Tools;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName Run
+ * @Description TODO
+ * @Author GY.C
+ * @Date 2020/3/22 11:01
+ * @Version 1.0
+ *
+ * getCompletedTaskCount 获取已经完成的任务
+ */
+public class Run {
+    public static void main(String[] args) {
+
+        Runnable runnable=()-> System.out.println("执行任务");
+
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 2, 5, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
+
+        pool.execute(runnable);
+        pool.execute(runnable);
+        pool.execute(runnable);
+
+        System.out.println(pool.getCompletedTaskCount());
+
+        Tools.sleep();
+        System.out.println(pool.getCompletedTaskCount());
+
+    }
+}
